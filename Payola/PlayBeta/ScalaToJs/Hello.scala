74fbd7b7c42a9a5ab167f3a69cc13dd36f5e0aa6
siroky <siroky.honza@seznam.cz>
2011-11-09 23:32:43 +0100

    s2js commit fix

---
+++ Payola/PlayBeta/ScalaToJs/Hello.scala
@@ -0,0 +1,24 @@
+package generated
+
+import browser._
+
+object Hello {
+    def main() {
+        val b = new B(4, "Ahoy")
+        alert(b.x(3))
+        alert(b.y("123*"))
+        alert("Hello world")
+    }
+}
+
+class A(val foo: Int, bar: String) {
+    def x(baz: Int): String = {
+        bar.charAt(baz).toString;
+    }
+}
+
+class B(foo: Int, bar: String) extends A(foo, bar) {
+    def y(baz: String): String = {
+        baz.charAt(foo).toString;
+    }
+}
