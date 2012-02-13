6f928891b93264b03e931bd670c31f6035b188b9
Jiri Helmich <jiri@helmich.cz>
2012-02-13 21:10:21 +0100

    RPC improvement - added support for post, urlencode, sync, ...

--- payola/s2js/runtime/src/main/scala/s2js/runtime/s2js/rpc.scala
+++ payola/s2js/runtime/src/main/scala/s2js/runtime/s2js/rpc.scala
@@ -3,7 +3,11 @@ package s2js.runtime.s2js
 import s2js.compiler.NativeJs
 
 object rpc {
-       @NativeJs("""xmlhttp.onreadystatechange=function()
+       @NativeJs("""
+
+                    var xmlhttp = XMLHttpRequest  ? new XMLHttpRequest : new ActiveXObject('Msxml2.XMLHTTP');
+
+                    xmlhttp.onreadystatechange=function()
                     {
                         if (xmlhttp.readyState==4 && xmlhttp.status==200)
                         {
@@ -13,8 +17,76 @@ object rpc {
                             faultCallback();
                         }
                     }
-                    xmlhttp.open(requestType,"/RPC",async);
-                    xmlhttp.send();
+
+                    var url = "/RPC";
+                    var encodedData = buildHttpQuery(params);
+
+                    if (requestType.toUpperCase() == "GET")
+                    {
+                        url += "?"+encodedData;
+                    }
+
+                    xmlhttp.open(requestType,url,false);
+
+                    if (requestType.toUpperCase() == "POST" || request)
+                    {
+                        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                        xmlhttp.send(encodedData);
+                    }else
+                    {
+                        xmlhttp.send();
+                    }
     """)
-    def CallRPC(callback: String => Unit, faultCallback: => Unit, procedureName: String, requestType: String = "GET", async: Boolean = true): Unit = null
+    def CallRPC(callback: String => Unit, faultCallback: => Unit, procedureName: String, params: Map[String, Object], requestType: String = "GET"): Unit = null
+
+       @NativeJs("""
+                    var xmlhttp = XMLHttpRequest  ? new XMLHttpRequest : new ActiveXObject('Msxml2.XMLHTTP');
+
+                    var data = null;
+
+                    xmlhttp.onreadystatechange=function()
+                    {
+                        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                        {
+                            data = xmlhttp.responseText;
+                        }
+                    }
+
+                    var url = "/RPC";
+                    var encodedData = buildHttpQuery(params);
+
+                    if (requestType.toUpperCase() == "GET")
+                    {
+                        url += "?"+encodedData;
+                    }
+
+                    xmlhttp.open(requestType,url,false);
+
+                    if (requestType.toUpperCase() == "POST")
+                    {
+                        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                        xmlhttp.send(encodedData);
+                    }else
+                    {
+                        xmlhttp.send();
+                    }
+                    return data;
+    """)
+    def CallRPCSync(procedureName: String, params:  Map[String, Object], requestType: String = "GET"): Unit = null
+
+    @NativeJs("""
+
+        var args = '';
+        if (Object.prototype.toString.call(params) === '[object Object]') {
+                var arr = [];
+                for (arg in params) {
+                        arr.push(encodeURIComponent(arg) + '=' + encodeURIComponent(params[arg]));
+                }
+                args = arr.join('&');
+        }
+
+        return args;
+
+    """)
+    private def buildHttpQuery(params: Map[String, Object]): String = null
 }
