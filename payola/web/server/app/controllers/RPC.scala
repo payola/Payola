1cfc487cb08494304b819aa74f7f586c4ce8c5e1
siroky <siroky.honza@seznam.cz>
2012-02-20 10:23:04 +0100

    Synchronous js post.

--- payola/web/server/app/controllers/RPC.scala
+++ payola/web/server/app/controllers/RPC.scala
@@ -4,7 +4,14 @@ import play.api.mvc._
 
 object RPC extends Controller
 {
-    def index = Action {
-        Ok("{rsp: \"wow\"}")
+    def index() = Action {request =>
+        val params = request.body match {
+            case AnyContentAsFormUrlEncoded(data) => data
+            case _ => Map.empty[String, Seq[String]]
+        };
+        val serializedParams = params.foldLeft("") {(buf, par) =>
+            buf + "%s: [%s], ".format(par._1, par._2.mkString(", "))
+        }
+        Ok("Response for received request: %s\n\n and params: %s".format(request.body.toString, serializedParams))
     }
 }
