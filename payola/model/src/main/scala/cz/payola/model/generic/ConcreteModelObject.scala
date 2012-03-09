a08cf6d6298804b657959601c7434c80ef0fc3d4
charlieMonroe <charliemonroe@Charlie-Monroes-MacBook-Pro.local>
2012-03-09 22:36:39 +0100

    Making objectID lazy-created.

--- payola/model/src/main/scala/cz/payola/model/generic/ConcreteModelObject.scala
+++ payola/model/src/main/scala/cz/payola/model/generic/ConcreteModelObject.scala
@@ -4,6 +4,19 @@ import java.util.UUID
 import cz.payola.common.model.ModelObject
 
 trait ConcreteModelObject extends ModelObject {
-    var objectID: String = UUID.randomUUID.toString
+    // Lazy objectID creation
+    var _objectID: String = null
+    
+    def objectID: String = {
+        if (_objectID == null){
+            // Hasn't been created yet, create it
+            _objectID = UUID.randomUUID.toString
+        }
+        _objectID
+    }
+
+    def objectID_=(objID: String) = _objectID = objID
+    
+    
 }
 
