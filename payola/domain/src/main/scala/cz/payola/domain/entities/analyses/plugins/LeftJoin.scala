3eb67cc3251dae99f9b2a7c94455bb34a926bae6
siroky <siroky.honza@seznam.cz>
2012-05-08 16:14:16 +0200

    Added left join parameter.

--- payola/domain/src/main/scala/cz/payola/domain/entities/analyses/plugins/LeftJoin.scala
+++ payola/domain/src/main/scala/cz/payola/domain/entities/analyses/plugins/LeftJoin.scala
@@ -5,7 +5,8 @@ import cz.payola.domain.rdf.Graph
 import cz.payola.domain.entities.analyses.parameters.{BooleanParameter, StringParameter}
 
 class LeftJoin extends Plugin("Left Join", 2, List(
-    new StringParameter("JoinPropertyURI", "")))
+    new StringParameter("JoinPropertyURI", ""),
+    new BooleanParameter("IsInner", true)))
 {
     def evaluate(instance: PluginInstance, inputs: IndexedSeq[Graph], progressReporter: Double => Unit): Graph = {
         // TODO
