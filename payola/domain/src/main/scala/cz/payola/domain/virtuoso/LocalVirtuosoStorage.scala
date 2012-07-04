9ba68fc0cf03795f4387d5088244ef8a0aeb41d4
siroky <siroky.honza@seznam.cz>
2012-07-04 15:55:18 +0200

    Preparing Virtuoso Storage for PayolaVirtuosoStoragePlugin.

--- payola/domain/src/main/scala/cz/payola/domain/virtuoso/LocalVirtuosoStorage.scala
+++
@@ -1,8 +0,0 @@
-package cz.payola.domain.virtuoso
-
-/** Creates a new VirtuosoStorage object with a local installation.
-  *
-  */
-object LocalVirtuosoStorage extends VirtuosoStorage("localhost", 8890, false, "dba", "dba") {
-    // TODO - load from config file
-}
