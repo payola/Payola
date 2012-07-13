620772501ee5cf2d9617475313fbf04638d6482b
charlieMonroe <charlie@charliemonroe.net>
2012-07-13 16:14:08 +0200

    Plugin creation.

---
+++ payola/web/shared/src/main/scala/cz/payola/web/shared/PluginManager.scala
@@ -0,0 +1,13 @@
+package cz.payola.web.shared
+
+/**
+  * Created with IntelliJ IDEA.
+  * User: charliemonroe
+  * Date: 7/13/12
+  * Time: 2:41 PM
+  * To change this template use File | Settings | File Templates.
+  */
+
+object PluginManager
+{
+}
