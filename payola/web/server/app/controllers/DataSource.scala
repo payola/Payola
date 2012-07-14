625dce6db4a866223336c4bd38e5c91410a1d564
charlieMonroe <charlie@charliemonroe.net>
2012-07-14 12:15:21 +0200

    Initial work on data sources, other bug fixes.

--- payola/web/server/app/controllers/DataSource.scala
+++ payola/web/server/app/controllers/DataSource.scala
@@ -4,9 +4,25 @@ import helpers.Secured
 import cz.payola.domain.entities.User
 import play.api.mvc.Request
 import cz.payola.web.shared.Payola
+import cz.payola.domain.entities.plugins.concrete.DataFetcher
 
 object DataSource extends PayolaController with Secured
 {
+    def create() = authenticated { user: User =>
+        Ok(views.html.datasource.create(user))
+    }
+
+    def delete(id: String) = authenticated { user: User =>
+        val ds: Option[cz.payola.domain.entities.plugins.DataSource] = Payola.model.dataSourceModel.getById(id)
+        ds.map { d =>
+            user.removeOwnedDataSource(d)
+            Payola.model.dataSourceModel.remove(d)
+            Redirect(routes.DataSource.list)
+        }.getOrElse {
+            NotFound(views.html.errors.err404("The data source does not exist."))
+        }
+    }
+
     def detail(id: String) = maybeAuthenticated { user: Option[User] =>
         Payola.model.dataSourceModel.getById(id).map { d =>
             Ok(views.html.datasource.detail(user, d))
@@ -15,7 +31,18 @@ object DataSource extends PayolaController with Secured
         }
     }
 
+    def edit(id: String) = authenticated { user: User =>
+        Payola.model.dataSourceModel.getById(id).map { d =>
+            val availableDataFetchers = Payola.model.pluginModel.getAccessibleToUser(Some(user)).filter(p => p.isInstanceOf[DataFetcher]).asInstanceOf[Seq[DataFetcher]]
+            Ok(views.html.datasource.edit(user, d, availableDataFetchers))
+        }.getOrElse {
+            NotFound(views.html.errors.err404("The data source does not exist."))
+        }
+    }
+
     def list() = authenticated { user: User =>
         Ok(views.html.datasource.list(user))
     }
+
+
 }
