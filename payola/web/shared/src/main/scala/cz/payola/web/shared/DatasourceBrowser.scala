9140aa8010e79c857dbf3d947d114156938af20f
siroky <siroky.honza@seznam.cz>
2012-07-16 16:24:52 +0200

    Data source browser is asynchronous, shows loading overlay. Removed unnecessary dependencies.

--- payola/web/shared/src/main/scala/cz/payola/web/shared/DatasourceBrowser.scala
+++ payola/web/shared/src/main/scala/cz/payola/web/shared/DatasourceBrowser.scala
@@ -1,23 +1,32 @@
 package cz.payola.web.shared
 
-import s2js.compiler.secured
+import s2js.compiler._
 import cz.payola.common.rdf.Graph
 import cz.payola.domain.entities.User
 import cz.payola.domain.entities.plugins.DataSource
+import scala.Some
 
 @remote object DataSourceBrowser
 {
     // TODO change user type from User to Option[User].
-    @secured def getInitialGraph(dataSourceId: String, user: User = null): Option[Graph] = {
-        getDataSource(dataSourceId, user).flatMap { dataSource =>
+    @secured @async def getInitialGraph(dataSourceId: String, user: User = null)
+        (successCallback: (Option[Graph] => Unit))
+        (failCallback: (Throwable => Unit)) {
+
+        val graph = getDataSource(dataSourceId, user).flatMap { dataSource =>
             val uri = dataSource.getFirstTriple.map(_.origin.uri)
             uri.map(dataSource.getNeighbourhood(_))
         }
+        successCallback(graph)
     }
 
     // TODO change user type from User to Option[User].
-    @secured def getNeighbourhood(dataSourceId: String, vertexURI: String, user: User = null): Option[Graph] = {
-        getDataSource(dataSourceId, user).map(_.getNeighbourhood(vertexURI, 1))
+    @secured @async def getNeighbourhood(dataSourceId: String, vertexURI: String, user: User = null)
+        (successCallback: (Option[Graph] => Unit))
+        (failCallback: (Throwable => Unit)) {
+
+        val graph = getDataSource(dataSourceId, user).map(_.getNeighbourhood(vertexURI, 1))
+        successCallback(graph)
     }
 
     private def getDataSource(dataSourceId: String, user: User): Option[DataSource] = {
