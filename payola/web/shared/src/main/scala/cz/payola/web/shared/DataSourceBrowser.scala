914952ffd1e6cf341772622326661b6c5d6b52ce
charlieMonroe <charlie@charliemonroe.net>
2012-07-16 19:10:44 +0200

    Adding DataSourceBrowser again

---
+++ payola/web/shared/src/main/scala/cz/payola/web/shared/DataSourceBrowser.scala
@@ -0,0 +1,32 @@
+package cz.payola.web.shared
+
+import s2js.compiler._
+import cz.payola.common.rdf.Graph
+import cz.payola.domain.entities.User
+import cz.payola.domain.entities.plugins.DataSource
+
+@remote object DataSourceBrowser
+{
+    @secured @async def getInitialGraph(dataSourceId: String, user: Option[User] = null)
+        (successCallback: (Option[Graph] => Unit))
+        (failCallback: (Throwable => Unit)) {
+
+        val graph = getDataSource(dataSourceId, user).flatMap { dataSource =>
+            val uri = dataSource.getFirstTriple.map(_.origin.uri)
+            uri.map(dataSource.getNeighbourhood(_))
+        }
+        successCallback(graph)
+    }
+
+    @secured @async def getNeighbourhood(dataSourceId: String, vertexURI: String, user: Option[User] = null)
+        (successCallback: (Option[Graph] => Unit))
+        (failCallback: (Throwable => Unit)) {
+
+        val graph = getDataSource(dataSourceId, user).map(_.getNeighbourhood(vertexURI, 1))
+        successCallback(graph)
+    }
+
+    private def getDataSource(dataSourceId: String, user: Option[User]): Option[DataSource] = {
+        Payola.model.dataSourceModel.getAccessibleToUserById(user, dataSourceId)
+    }
+}
