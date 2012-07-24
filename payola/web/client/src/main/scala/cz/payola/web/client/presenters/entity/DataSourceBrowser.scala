601eb85008d4446826ee795ad0d94473ad0b2b38
Ondra Heřmánek <ondra.hermanek@gmail.com>
2012-07-24 11:03:45 +0200

    compilation fixed

--- payola/web/client/src/main/scala/cz/payola/web/client/presenters/entity/DataSourceBrowser.scala
+++ payola/web/client/src/main/scala/cz/payola/web/client/presenters/entity/DataSourceBrowser.scala
@@ -5,7 +5,6 @@ import s2js.adapters.js.dom
 import cz.payola.web.shared.managers._
 import cz.payola.web.client.events.BrowserEventArgs
 import cz.payola.web.client.views.VertexEventArgs
-import cz.payola.web.client.views.entity.dataSource._
 import cz.payola.web.client.Presenter
 import cz.payola.web.client.presenters.graph.GraphPresenter
 import cz.payola.web.client.views.entity.DataSourceView
