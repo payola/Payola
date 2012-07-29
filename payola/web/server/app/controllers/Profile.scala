43c29cade14da3647ff51ae9be9f1168aeb27f03
Jiri Helmich <jiri@helmich.cz>
2012-07-29 21:18:46 +0200

    #131 groups moved to separated controller, security checked

--- payola/web/server/app/controllers/Profile.scala
+++ payola/web/server/app/controllers/Profile.scala
@@ -5,7 +5,6 @@ import play.api.data._
 import play.api.data.Forms._
 import views._
 import cz.payola.domain.entities._
-import play.api.mvc._
 import cz.payola.web.shared.Payola
 
 object Profile extends PayolaController with Secured
@@ -33,10 +32,6 @@ object Profile extends PayolaController with Secured
         })
     )
 
-    val groupForm = Form(
-        "name" -> text
-    )
-
     // TODO is the username necessary here? A user may edit only his own profile...
     def edit(username: String) = authenticated { user =>
         Ok(html.Profile.edit(user, profileForm))
@@ -45,107 +40,6 @@ object Profile extends PayolaController with Secured
     // TODO is the username necessary here? A user may edit only his own profile...
     def save(username: String) = authenticated { user =>
         Ok("TODO")
-    }
-
-    /** Shows a group creation page.
-      *
-      * @return Page with group creation.
-      */
-    def createGroup = authenticated { user =>
-        Ok(html.Profile.createGroup(user, groupForm))
-    }
-
-    /** Save a newly created group.
-      *
-      * @return Redirects to group listing.
-      */
-    def saveCreateGroup = authenticatedWithRequest { (user, request) =>
-        val name = groupForm.bindFromRequest()(request).get
-        val group = Payola.model.groupModel.create(name, user)
-
-        if (group != null)
-        {
-            Redirect(routes.Profile.editGroup(group.id)).flashing("success" -> "The group has been sucessfully created.")
-        }else
-        {
-            Redirect(routes.Profile.listGroups()).flashing("error" -> "The group could not be created.")
-        }
-    }
-
-    /** Saves a group with id.
-      *
-      * @param id ID of group.
-      * @return Redirects to the user index page.
-      */
-    def saveGroup(id: String) = authenticatedWithRequest{ (user, request) =>
-
-        val data = request.body match {
-            case AnyContentAsFormUrlEncoded(data) => data
-            case _ => Map.empty[String, Seq[String]]
-        }
-
-        val membersNew = data.getOrElse("members",Nil).flatMap{ s => s.split(',') }.flatMap{ uId => Payola.model.userModel.getById(uId) }
-        val group = user.ownedGroups.find(_.id == id)
-
-        if (group.isDefined)
-        {
-            val g = group.get
-            g.name = data.getOrElse("name", Nil).head
-            g.members.diff(membersNew).map{m =>
-                g.removeMember(m)
-            }
-
-            membersNew.diff(g.members).map{ m =>
-                g.addMember(m)
-            }
-
-            Payola.model.groupModel.persist(g)
-            Redirect(routes.Profile.index(user.email)).flashing("success" -> "The group has been sucessfully saved.")
-        }else{
-            Forbidden
-        }
-    }
-
-    /** Edit a group.
-      *
-      * @param id ID of a group to edit.
-      * @return Redirects to the group listing.
-      */
-    def editGroup(id: String) = authenticatedWithRequest{ (user, request) =>
-        val g = user.ownedGroups.find(_.id == id)
-
-        if (g.isDefined)
-        {
-            val allUsers = Payola.model.userModel.getAll()
-
-            Ok(views.html.Profile.editGroup(user, g.get, allUsers))
-        }else{
-            NotFound("The group does not exist.")
-        }
-    }
-
-    /** Shows the listing page for groups.
-      *
-      * @return Listing page for groups.
-      */
-    def listGroups = authenticatedWithRequest( (user, request) =>
-        Ok(views.html.Profile.listGroups(user)(request.flash))
-    )
-
-    /** Deletes group with id.
-      *
-      * @param id ID of the group to delete.
-      * @return Redirects to the group listing.
-      */
-    def deleteGroup(id: String) = authenticated{ user =>
-        val group = user.ownedGroups.find(_.id == id)
-        Redirect(routes.Profile.listGroups()).flashing(
-            if (group.map(g => Payola.model.groupModel.remove(g)).getOrElse(false)) {
-                "success" -> "The group has been successfully deleted."
-            } else {
-                "error" -> "The group could not been deleted."
-            }
-        )
     }
 
     /** Shows the create page for plugin.
