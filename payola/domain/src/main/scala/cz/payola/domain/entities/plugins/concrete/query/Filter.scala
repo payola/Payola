296eff3f16126cff9135ae3af3140736a12bbbc2
siroky <siroky.honza@seznam.cz>
2012-08-31 15:29:27 +0200

    Fixed data tests, fixed #212.

--- payola/domain/src/main/scala/cz/payola/domain/entities/plugins/concrete/query/Filter.scala
+++ payola/domain/src/main/scala/cz/payola/domain/entities/plugins/concrete/query/Filter.scala
@@ -4,42 +4,43 @@ import scala.collection.immutable
 import cz.payola.domain.IDGenerator
 import cz.payola.domain.entities.plugins._
 import cz.payola.domain.entities.plugins.parameters.StringParameter
+import cz.payola.domain.sparql
 import cz.payola.domain.sparql._
 
-class Selection(name: String, inputCount: Int, parameters: immutable.Seq[Parameter[_]], id: String)
+class Filter(name: String, inputCount: Int, parameters: immutable.Seq[Parameter[_]], id: String)
     extends Construct(name, inputCount, parameters, id)
 {
     def this() = {
-        this("Selection", 1, List(
-            new StringParameter(Selection.propertyURIParameter, "", false),
-            new StringParameter(Selection.operatorParameter, "", false),
-            new StringParameter(Selection.valueParameter, "", false)
+        this("Filter", 1, List(
+            new StringParameter(Filter.propertyURIParameter, "", false),
+            new StringParameter(Filter.operatorParameter, "", false),
+            new StringParameter(Filter.valueParameter, "", false)
         ), IDGenerator.newId)
     }
 
     def getPropertyURI(instance: PluginInstance): Option[String] = {
-        instance.getStringParameter(Selection.propertyURIParameter)
+        instance.getStringParameter(Filter.propertyURIParameter)
     }
 
     def getOperator(instance: PluginInstance): Option[String] = {
-        instance.getStringParameter(Selection.operatorParameter)
+        instance.getStringParameter(Filter.operatorParameter)
     }
 
     def getValue(instance: PluginInstance): Option[String] = {
-        instance.getStringParameter(Selection.valueParameter)
+        instance.getStringParameter(Filter.valueParameter)
     }
 
     def getConstructQuery(instance: PluginInstance, subject: Subject, variableGetter: () => Variable) = {
         usingDefined(getPropertyURI(instance), getOperator(instance), getValue(instance)) { (u, o, v) =>
             val objectVariable = variableGetter()
             val triples = List(TriplePattern(subject, Uri(u), objectVariable))
-            val filters = List(Filter(objectVariable + " " + o + " " + v))
+            val filters = List(sparql.Filter(objectVariable + " " + o + " " + v))
             ConstructQuery(triples, Some(GraphPattern(triples, filters = filters)))
         }
     }
 }
 
-object Selection
+object Filter
 {
     val propertyURIParameter = "Property URI"
 
