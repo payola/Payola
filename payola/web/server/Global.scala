15f7ed8cb94d4609334f3c2343f704e97dedf3d4
Jiri Helmich <jiri@helmich.cz>
2012-09-03 13:19:19 +0200

    Global err handlers

---
+++ payola/web/server/Global.scala
@@ -0,0 +1,12 @@
+import controllers.routes
+import play.api.GlobalSettings
+import play.api.mvc._
+
+object Global extends GlobalSettings {
+    override def onError(request : play.api.mvc.RequestHeader, ex : scala.Throwable) : play.api.mvc.Result = {
+        play.api.mvc.Results.Redirect(routes.ErrorHandler.error())
+    }
+    override def onHandlerNotFound(request: RequestHeader): Result = {
+        play.api.mvc.Results.Redirect(routes.ErrorHandler.notFound())
+    }
+}
