070c43c8ff2019360c28f8ad2779b037280871a8
Ondra Kudlacek <kudl.ondr@gmail.com>
2013-11-18 20:22:10 +0100

    Added userCustomization class ordering to DB and ordering support to UI.

--- payola/web/client/src/main/scala/cz/payola/web/client/views/bootstrap/buttonedListItem.scala
+++ payola/web/client/src/main/scala/cz/payola/web/client/views/bootstrap/buttonedListItem.scala
@@ -3,19 +3,57 @@ package cz.payola.web.client.views.bootstrap
 import cz.payola.web.client.views.elements.lists.ListItem
 import cz.payola.web.client.View
 import cz.payola.web.client.events.SimpleUnitEvent
+import cz.payola.web.client.views.elements._
+import cz.payola.web.client.views.ComposedView
+import scala.collection.mutable.ListBuffer
 
-class RemovableListItem(incon: String, subViews: Seq[View] = Nil, cssClass: String = "") extends ListItem(subViews, cssClass) {
+class ButtonedListItem(icon: String, content: Seq[View], cssClass: String = "") extends ComposedView {
 
-    private val button = new Anchor(List(new Icon(icon)))
+    val buttonEvent = new SimpleUnitEvent[this.type]
 
-    button.mouseClicked += { e =>
-        changed.triggerDirectly(this)
+    val extendedContent = new ListBuffer[View]()
+    extendedContent ++= content
+    extendedContent += button
+
+    val listItem = new ListItem(List(new Div(extendedContent.toList)), cssClass)
+
+    listItem.mouseOut += { e =>
+        button.hide()
         false
     }
 
-    def createSubViews(): Seq[View] = {
-        subViews ++ remove
+    listItem.mouseMoved += { e =>
+        button.show("")
+        false
     }
 
-    val changed = new SimpleUnitEvent[this.type]
+    private val button = new Anchor(List(new Icon(icon)))
+
+    button.hide()
+
+    button.mouseClicked += { e =>
+        buttonEvent.triggerDirectly(this)
+        false
+    }
+
+    def getAttribute(name: String) = listItem.getAttribute(name)
+
+    def setAttribute(name: String, value: String): this.type = {
+        listItem.setAttribute(name, value)
+        this
+    }
+
+    //def createSubViews = List(listItem)
+
+    def createSubViews = List(listItem)
+
+    def removeCssClass(cssClass: String): this.type = {
+        listItem.removeCssClass(cssClass)
+        this
+    }
+
+    def addCssClass(cssClass: String): this.type = {
+        listItem.addCssClass(cssClass)
+        this
+    }
 }
