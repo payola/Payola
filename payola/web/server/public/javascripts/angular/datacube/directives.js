0d9066b4f8688839176f421329992363116d4f75
Jiri Helmich <jiri@helmich.cz>
2014-05-03 21:24:22 +0200

    czRegionMap, mapping out

--- payola/web/server/public/javascripts/angular/datacube/directives.js
+++ payola/web/server/public/javascripts/angular/datacube/directives.js
@@ -1,3 +1,144 @@
+'use strict';
+
+/* Directives */
+
+angular.module('dataCube.directives', [])
+    .directive('czRegionMap', [function () {
+
+        function link(scope, element, attr) {
+            scope.$watch("data", updateFunc, true);
+            var dataTitle = scope.title;
+            var regionId = null;
+
+            var el = element.find("path");
+            for (var i = 0; i < el.length; i++) {
+                el[i].setAttribute("region", i)
+            }
+
+            var mapping = [
+            ];
+
+            element.find("path").on("mouseover", function (event) {
+                RegionOver(this, event)
+
+            });
+
+            element.find("path").on("mousemove", function (e) {
+                scope.ttLeft = e.pageX + 20;
+                scope.ttTop = e.pageY + 20;
+                scope.ttShow = "block";
+                scope.$apply();
+
+            });
+            element.find("select").on("change", function (e) {
+
+                settingsChanged()
+
+            });
+
+            element.find("path").on("click", function () {
+                RegionClick(this);
+            });
+
+            element.find("path").on("mouseleave", function () {
+                RegionOut(this);
+
+            });
+
+            scope.change = function () {
+                scope.showSimple = scope.mapMode == 0;
+
+                settingsChanged();
+            };
+
+            function getVal(value1, value2) {
+
+                switch (scope.dataOperation.id) {
+                    case 0:
+                        return value2 - value1;
+                        break;
+                    case 1:
+
+                        return value2 / value1;
+                        break;
+                    case 2:
+
+                        return value1 + value2;
+                        break
+                }
+
+            }
+
+            function settingsChanged() {
+
+                var dataObject = scope.data.data[0].data;
+                var newData = new Array();
+                var dataObject;
+                if (scope.mapMode == 0) {
+
+                    dataObject = scope.data.data[scope.dataSetSimple.id].data;
+
+                    for (var i = 0; i < dataObject.length; i++) {
+                        var curObj = dataObject[i];
+                        var lau = curObj.tickValue.substr(curObj.tickValue.lastIndexOf("/") + 1);
+                        newData.push([lau, curObj.y])
+                    }
+                } else {
+
+                    var dataObj1 = scope.data.data[scope.dataSet1.id].data;
+                    var dataObj2 = scope.data.data[scope.dataSet2.id].data;
+                    for (var i = 0; i < dataObj1.length; i++) {
+                        for (var e = 0; e < dataObj2.length; e++) {
+                            if (dataObj1[i].tickValue == dataObj2[e].tickValue) {
+                                var titleText = scope.dataSet1.name + ": " + dataObj1[i].y + "\n" + scope.dataSet2.name + ": " + dataObj2[e].y + "\n\n";
+
+                                newData.push([dataObj1[i].tickValue.substr(dataObj1[i].tickValue.lastIndexOf("/") + 1), getVal(dataObj1[i].y,
+                                    dataObj2[e].y), titleText]);
+                                break;
+                            }
+                        }
+
+                    }
+                }
+
+                SetDataArray(newData)
+            }
+
+            function updateFunc(oldval, newval) {
+
+                dataTitle = scope.data.title;
+                scope.mapMode = 0;
+
+                scope.showSimple = 1;
+                scope.dataSets = new Array();
+                scope.dataOperations = [
+                    {name: 'Difference', id: 0},
+                    {name: 'Ratio', id: 1},
+                    {name: 'Sum', id: 2}
+                ];
+                scope.dataOperation = scope.dataOperations[0];
+
+                if (scope.data.data.length > 1) {
+                    scope.hideOptions = false;
+                } else {
+                    scope.hideOptions = true;
+                }
+                for (var i = 0; i < scope.data.data.length; i++) {
+
+                    var n = scope.data.data[i].name;
+
+                    scope.dataSets.push({name: n, id: i});
+                    if (i == 0) {
+                        scope.dataSetSimple = scope.dataSets[0];
+                        scope.dataSet1 = scope.dataSets[0];
+
+                    } else if (i == 1) {
+                        scope.dataSet2 = scope.dataSets[1];
+                    }
+                }
+                settingsChanged();
+
+            }
 
             function SetTooltip(title, headline, text, text2) {
                 if (text == null) {
