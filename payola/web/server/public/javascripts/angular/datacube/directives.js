ede42328d5605410a2a92bf7cbbea401ecb16e23
Jiri Helmich <jiri@helmich.cz>
2014-05-03 20:51:23 +0200

    new czRegionMap version

--- payola/web/server/public/javascripts/angular/datacube/directives.js
+++ payola/web/server/public/javascripts/angular/datacube/directives.js
@@ -20,61 +20,61 @@ angular.module('dataCube.directives', [])
                 "Praha", 10, "CZ0100",
                 "Beroun", 46, "CZ0202",
                 "Blansko", 7, "CZ0641",
-                "Brno-m?sto", 38, "CZ0642",
+                "Brno-město", 38, "CZ0642",
                 "Benešov", 53, "CZ0201",
                 "Brno-venkov", 74, "CZ0643",
                 "Bruntál", 3, "CZ0801",
-                "B?eclav", 48, "CZ064",
-                "?eské Bud?jovice", 36, "CZ0311",
+                "Bøeclav", 48, "CZ064",
+                "České Budějovice", 36, "CZ0311",
                 "Cheb", 0, "CZ0411",
-                "?eský Krumlov", 67, "CZ0312",
-                "?eská Lípa", 6, "CZ0511",
+                "Český Krumlov", 67, "CZ0312",
+                "Česká Lípa", 6, "CZ0511",
                 "Chrudim", 23, "CZ0531",
                 "Chomutov", 14, "CZ0422",
-                "D??ín", 18, "CZ0421",
+                "Děšín", 18, "CZ0421",
                 "Domažlice", 32, "CZ0321",
                 "Frýdek-Místek", 75, "CZ0802	",
-                "Havlí?k?v Brod", 39, "CZ0631",
+                "Havlíčkův Brod", 39, "CZ0631",
                 "Hradec Králové", 64, "CZ0521",
                 "Hodonín", 54, "CZ0645",
-                "Ji?ín", 76, "CZ0522",
+                "Jičín", 76, "CZ0522",
                 "Jeseník", 60, "CZ0711",
-                "Jind?ich?v Hradec", 42, "CZ0313",
+                "Jindřichův Hradec", 42, "CZ0313",
                 "Jihlava", 4, "CZ0632",
                 "Jablonec nad Nisou", 25, "CZ0512",
                 "Kutná Hora", 52, "CZ0205",
                 "Karviná", 73, "CZ0803	",
                 "Kladno", 50, "CZ0203",
-                "Krom??íž", 2, "CZ0721",
+                "Kroměříž", 2, "CZ0721",
                 "Kolín", 51, "CZ0204",
                 "Klatovy", 33, "CZ0322",
                 "Karlovy Vary", 15, "CZ0412",
                 "Liberec", 22, "CZ0513",
                 "Louny", 21, "CZ0424",
-                "Litom??ice", 16, "CZ0423",
+                "Litoměřice", 16, "CZ0423",
                 "Mladá Boleslav", 58, "CZ0207",
-                "M?lník", 59, "CZ0206",
+                "Mělník", 59, "CZ0206",
                 "Most", 20, "CZ0425",
                 "Náchod", 5, "CZ0523",
                 "Nymburk", 57, "CZ0208",
-                "Nový Ji?ín", 66, "CZ0804",
+                "Nový Jičín", 66, "CZ0804",
                 "Olomouc", 63, "CZ0712",
                 "Opava", 69, "CZ0805",
                 "Ostrava", 72, "CZ0806",
-                "P?íbram", 47, "CZ020B",
-                "Pelh?imov", 41, "CZ0633",
+                "Příbram", 47, "CZ020B",
+                "Pelhřimov", 41, "CZ0633",
                 "Praha-východ", 24, "CZ0209",
                 "Písek", 35, "CZ0314",
-                "Plze?-jih", 37, "CZ0324",
-                "Plze?-m?sto", 40, "CZ0323",
-                "P?erov", 1, "CZ0714",
-                "Plze?-sever", 43, "CZ0325",
+                "Plzeň-jih", 37, "CZ0324",
+                "Plzeň-město", 40, "CZ0323",
+                "Přerov", 1, "CZ0714",
+                "Plzeň-sever", 43, "CZ0325",
                 "Prachatice", 68, "CZ0315",
                 "Pardubice", 8, "CZ0532",
-                "Prost?jov", 62, "CZ0713",
+                "Prostějov", 62, "CZ0713",
                 "Praha-západ", 49, "CZ020A",
                 "Rakovník", 45, "CZ020C",
-                "Rychnov nad Kn?žnou", 27, "CZ0524",
+                "Rychnov nad Kněžnou", 27, "CZ0524",
                 "Rokycany", 12, "CZ0326",
                 "Semily", 26, "CZ0514",
                 "Sokolov", 13, "CZ0413",
@@ -84,20 +84,21 @@ angular.module('dataCube.directives', [])
                 "Tábor", 34, "CZ0317",
                 "Tachov", 31, "CZ0327",
                 "Teplice", 11, "CZ0426",
-                "T?ebí?", 55, "CZ0634",
+                "Třebíè", 55, "CZ0634",
                 "Trutnov", 17, "CZ0525",
-                "Uherské Hradišt?", 44, "CZ0722",
+                "Uherské Hradiště", 44, "CZ0722",
                 "Ústí nad Labem", 19, "CZ0427",
                 "Ústí nad Orlicí", 71, "CZ0534",
                 "Vsetín", 28, "CZ0723",
                 "Vyškov", 29, "CZ0646",
                 "Zlín", 70, "CZ0724",
                 "Znojmo", 56, "CZ0647",
-                "Ž?ár nad Sázavou", 9, "CZ0635"
+                "Žďár nad Sázavou", 9, "CZ0635"
             ];
 
             element.find("path").on("mouseover", function (event) {
                 RegionOver(this, event)
+                
             });
 
             element.find("path").on("mousemove", function (e) {
@@ -105,6 +106,11 @@ angular.module('dataCube.directives', [])
                 scope.ttTop = e.pageY + 20;
                 scope.ttShow = "block";
                 scope.$apply();
+
+            });
+            element.find("select").on("change", function (e) {
+                
+                        settingsChanged()
 
             });
 
@@ -116,27 +122,107 @@ angular.module('dataCube.directives', [])
                 RegionOut(this);
 
             });
+            
+            scope.change=function(){
+                scope.showSimple=scope.mapMode==0
+                
+                
+                settingsChanged();
+            }
+            function getVal(value1,value2){
+                
+                switch(scope.dataOperation.id){
+                    case 0:
+                        return value2-value1;
+                        break;
+                    case 1:
+                        
+                        return value2/value1
+                        break
+                    case 2:
+                        
+                        return value1+value2;
+                        break
+                }
+                
+            }
+            function settingsChanged(){
+             
+                var dataObject=scope.data.data[0].data;
+                var newData=new Array();
+                var dataObject;
+                if(scope.mapMode==0){
+                    
+                    
+                    dataObject=scope.data.data[scope.dataSetSimple.id].data
 
+
+
+                    for(var i=0;i<dataObject.length;i++){
+                        var curObj=dataObject[i];
+                        var lau=curObj.tickValue.substr( curObj.tickValue.lastIndexOf("/")+1 )
+                        newData.push([lau,curObj.y])
+                    }
+                }else{
+                    
+                    var dataObj1=scope.data.data[scope.dataSet1.id].data
+                    var dataObj2=scope.data.data[scope.dataSet2.id].data
+                    for(var i=0;i<dataObj1.length;i++){
+                        for(var e=0;e<dataObj2.length;e++){
+                            if(dataObj1[i].tickValue==dataObj2[e].tickValue){
+                                var titleText=scope.dataSet1.name+": "+dataObj1[i].y+"\n"+scope.dataSet2.name+": "+dataObj2[e].y+"\n\n";
+                                
+                                newData.push([dataObj1[i].tickValue.substr( dataObj1[i].tickValue.lastIndexOf("/")+1 ),getVal(dataObj1[i].y,dataObj2[e].y),titleText]);
+                                break;
+                            }
+                        }
+                        
+                    }
+                }
+                
+                
+                SetDataArray(newData)
+            }
             function updateFunc(oldval, newval) {
 
                 dataTitle = scope.data.title;
-
-                var dataObject=scope.data.data[0].data;
-                var newData=new Array();
-                for(var i=0;i<dataObject.length;i++){
-                    var curObj=dataObject[i];
-                    var lau=curObj.tickValue.substr( curObj.tickValue.lastIndexOf("/")+1 )
-                    newData.push([lau,curObj.y])
+                scope.mapMode=0
+                
+                scope.showSimple=1
+                scope.dataSets=new Array();
+                scope.dataOperations=[{name:'Difference',id:0},{name:'Ratio',id:1},{name:'Sum',id:2}]
+                scope.dataOperation=scope.dataOperations[0]
+                
+                if(scope.data.data.length>1){
+                    scope.hideOptions=false
+                }else{
+                    scope.hideOptions=true
                 }
-
-
-                SetDataArray(newData, 0, 1)
+                for(var i=0;i<scope.data.data.length;i++){
+                    
+                    var n=scope.data.data[i].name;
+                    
+                    
+                    scope.dataSets.push({name:n,id:i})
+                    if(i==0){
+                        scope.dataSetSimple=scope.dataSets[0]
+                        scope.dataSet1=scope.dataSets[0]
+                        
+                        
+                    }else if(i==1){
+                        scope.dataSet2=scope.dataSets[1]
+                    }
+                }
+                settingsChanged();
 
             }
 
-            function SetTooltip(title, headline, text) {
+            function SetTooltip(title, headline, text,text2) {
                 if (text == null) {
                     text = "";
+                }
+                if(text2==null){
+                    text2="";
                 }
                 if (title != null && title != "") {
                     title += " - ";
@@ -145,7 +231,29 @@ angular.module('dataCube.directives', [])
                 }
                 scope.title = title
                 scope.headline = headline
-                scope.text = text;
+                
+                if(text!=""){
+                if(scope.mapMode==1){
+                    var inc="";
+                    if(scope.dataOperation.id==1){
+                        if(text>=1){
+                            inc="+";
+                        }
+                        scope.text = "Change: "+inc +Math.round((Number(text)*100-100)*100)/100+"%\n\n"+text2;
+                    }else if(scope.dataOperation.id==0){
+                        if(text>=0){
+                            inc="+"
+                        }
+                        scope.text = "Difference: "+inc +text+"\n\n"+text2;
+                    }else{
+                        scope.text = "Sum: " +text+"\n\n"+text2;
+                    }
+                }else{
+                    scope.text = text+"\n\n"+text2;
+                }
+                }else{
+                    scope.text="";
+                }
 
                 scope.$apply();
 
@@ -159,7 +267,7 @@ angular.module('dataCube.directives', [])
             }
 
             function RegionClick(object) {
-                alert('todo graf')
+                //alert('todo graf')
                 //drawGraph();
             }
 
@@ -173,7 +281,7 @@ angular.module('dataCube.directives', [])
                 scope.$apply();
 
                 SetTooltip(dataTitle, GetRegionName(angular.element(object).attr("region")),
-                    angular.element(object).attr("name"));
+                    angular.element(object).attr("name"),angular.element(object).attr("title"));
             }
 
             function GetRegionName(id) {
@@ -204,35 +312,68 @@ angular.module('dataCube.directives', [])
             function GetRange(array, index) {
                 var min = 9999999999;
                 var max = -9999999999;
-
+                var sum=0;
                 for (var i = 0; i < array.length; i++) {
-
+                    sum+=Number(array[i][index]);
                     min = Math.min(min, Number(array[i][index]));
                     max = Math.max(max, Number(array[i][index]));
                 }
-                return [min, max];
+                var avg=sum/array.length;
+                var mm=Math.min(avg-min,max-avg);
+                return [min,max, avg-mm, avg+mm];
             }
 
-            function SetDataArray(list, indexId, indexData, range) {
-
+            function SetDataArray(list, range) {
+                var indexId=0
+                var indexData=1
                 var r = GetRange(list, indexData);
 
-                scope.barLow = r[0];
-                scope.barHigh = r[1];
-                //scope.$apply();
+                
+                
+                
+                if(scope.mapMode==1 && scope.dataOperation.id==1){
+                    scope.barLow = Math.round((r[0]*100-100)*100)/100;
+                    scope.barHigh = Math.round((r[1]*100-100)*100)/100;
+                    if(scope.barLow>=0){
+                        scope.barLow="+"+scope.barLow;
+                    }
+                    if(scope.barHigh>=0){
+                        scope.barHigh="+"+scope.barHigh;
+                    }
+                    scope.barLow+="%"
+                    scope.barHigh+="%"
+                    
+                }else{
+                    scope.barLow = Math.round(r[0]*100)/100;
+                    scope.barHigh = Math.round(r[1]*100)/100;
+                    if(scope.mapMode==1 && scope.dataOperation.id==0){
+                        if(scope.barLow>=0){
+                        scope.barLow="+"+scope.barLow;
+                    }
+                    if(scope.barHigh>=0){
+                        scope.barHigh="+"+scope.barHigh;
+                    }
+                    }
+                }
+                
 
                 if (range == null) {
                     range = r;
+                    range =[r[2],r[3]]
                 }
-
+                
+                for (var i = 0; i<mapping.length;i+=3){
+                    SetRegionColor(mapping[i+2],"#555555");
+                }
                 for (var i = 0; i < list.length; i++) {
-
-                    SetRegionColor(list[i][indexId], GetColor(range, list[i][indexData]), list[i][indexData]);
+                    
+                    SetRegionColor(list[i][indexId], GetColor(range, list[i][indexData]), list[i][indexData],list[i][indexData+1]);
                 }
-
+scope.$apply();
             }
 
-            function SetRegionColor(id, color, value) {
+            function SetRegionColor(id, color, value,value2) {
+                
                 var tmp = id;
                 if (Number(id).toString() == "NaN") {
                     id = GetLauRegionId(id);
@@ -253,6 +394,7 @@ angular.module('dataCube.directives', [])
 
                             angular.element(el[i]).attr("fill", color)
                             angular.element(el[i]).attr("name", value)
+                            angular.element(el[i]).attr("title", value2)
                         }
                     }
                 } catch (e) {
@@ -262,6 +404,9 @@ angular.module('dataCube.directives', [])
 
             function GetColor(range, value) {
                 var index = Math.min(1, (value - range[0]) / (range[1] - range[0]));
+                if(index.toString()=="NaN"){
+                    index=1
+                }
                 var g = Math.max(0, Math.min(255, Math.round(index * 255))).toString(16);
                 var r = Math.max(0, Math.min(255, Math.round(255 - index * 255))).toString(16);
                 if (r.length == 1) {
@@ -270,6 +415,9 @@ angular.module('dataCube.directives', [])
                 if (g.length == 1) {
                     g = "0" + g;
                 }
+                if(r.toString()=="NaN"){
+                    alert(index)
+                }
                 return "#" + r + g + "00";
             }
 
@@ -277,7 +425,7 @@ angular.module('dataCube.directives', [])
 
         return {
             restrict: 'E',
-            templateUrl: '/assets/javascripts/angular/datacube/partials/CZMap.html',
+            templateUrl: '/assets/javascripts/angular/datacube/partials/CZMap.html',         
             link: link,
             scope: {data: "="}
         }
